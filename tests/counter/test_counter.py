a85858213ce7b0d390dd3172a15a09479250e2da
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-07 20:10:14 -0300

    requisito 10 provavelmente concluído

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    'Para a palavra javascript a função deve retornar o número 122'
+    assert count_ocurrences("data/jobs.csv", "Javascript") == 122

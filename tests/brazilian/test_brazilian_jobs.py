8c24283490d65413d3d2d5a28b3e68489318066f
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-07 20:59:58 -0300

    requisito 11 concluído

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    'as chaves do dict salário, título e tipo devem retornar em inglês'
+    headers, *content = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert "title" and "salary" and "type" in headers

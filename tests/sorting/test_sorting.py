616583a5db1fddd504df5bf0510c2e01dbf2ebb4
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-08 04:08:17 -0300

    requisitos bonus concluídos

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,8 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
+import pytest
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(ValueError, match="invalid sorting criteria"):
+        sort_by(read("data/jobs.csv"), "max_salari")
